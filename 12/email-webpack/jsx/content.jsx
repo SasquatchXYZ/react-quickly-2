47bbe307e0ad99959b2595d9c00128443e2e48c6
SasquatchXYZ <dalton.ricker.2011@graduate.usmma.edu>
2019-02-02 11:12:35 -0500

    Content added

--- 12/email-webpack/jsx/content.jsx
+++ 12/email-webpack/jsx/content.jsx
@@ -2,6 +2,66 @@ const React = require('react');
 const ReactDOM = require('react-dom');
 
 class Content extends React.Component {
+  prompt = 'Please enter your email to win $1,000,000.';
+
+  submit = () => {
+    const emailAddress = this.refs.emailAddress;
+    const comments = this.refs.comments;
+
+    console.log(ReactDOM.findDOMNode(emailAddress).value);
+    console.log(ReactDOM.findDOMNode(comments).value);
+  };
+
+  render() {
+    return (
+      <div className="well">
+        <p>{this.prompt}</p>
+        <div className="form-group">
+          Email: <input ref="emailAddress" className="form-control" type="text" placeholder="hi@azat.co"/>
+        </div>
+        <div className="form-group">
+          Comments: <textarea ref="comments" className="form-control" placeholder="I like your website!"/>
+        </div>
+        <div className="form-group">
+          <a className="btn btn-primary" value="submit" onClick={this.submit}>Submit</a>
+        </div>
+      </div>
+    )
+  }
 }
+
+/*class Content extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.submit = this.submit.bind(this);
+    this.prompt = 'Please enter your email to win $1,000,000.';
+  }
+
+  submit = () => {
+    const emailAddress = this.refs.emailAddress;
+    const comments = this.refs.comments;
+
+    console.log(ReactDOM.findDOMNode(emailAddress).value);
+    console.log(ReactDOM.findDOMNode(comments).value);
+  };
+
+  render() {
+    return (
+      <div className="well">
+        <p>{this.prompt}</p>
+        <div className="form-group">
+          Email: <input ref="emailAddress" className="form-control" type="text" placeholder="hi@azat.co"/>
+        </div>
+        <div className="form-group">
+          Comments: <textarea ref="comments" className="form-control" placeholder="I like your website!"/>
+        </div>
+        <div className="form-group">
+          <a className="btn btn-primary" value="submit" onClick={this.submit}>Submit</a>
+        </div>
+      </div>
+    )
+  }
+}*/
 
 module.exports = Content;
