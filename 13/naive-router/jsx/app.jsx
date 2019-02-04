8c631cf9c6ae8bb82be9efad1343ff4840cb08cf
SasquatchXYZ <dalton.ricker.2011@graduate.usmma.edu>
2019-02-04 12:39:29 -0500

    well that was a bit of a battle...

--- 13/naive-router/jsx/app.jsx
+++ 13/naive-router/jsx/app.jsx
@@ -1,15 +1,15 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import Router from './router';
+const React = require('react');
+const ReactDOM = require('react-dom');
+const Router = require('./router.jsx');
 
 const mapping = {
-  '#profile': <div>Profile (<a href="#">Home</a>)</div>,
-  '#accounts': <div>Accounts (<a href="#">Home</a>)</div>,
+  '#profile': <div>Profile (<a href="#">home</a>)</div>,
+  '#accounts': <div>Accounts (<a href="#">home</a>)</div>,
   '*': <div>Dashboard<br/>
     <a href="#profile">Profile</a>
     <br/>
-    <a href="#accounts">Acoounts</a>
-  </div>,
+    <a href="#accounts">Accounts</a>
+  </div>
 };
 
 ReactDOM.render(
