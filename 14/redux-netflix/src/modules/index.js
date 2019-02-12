3cef901d6e0109e76b9b3dd9547436d27e45cb47
SasquatchXYZ <dalton.ricker.2011@graduate.usmma.edu>
2019-02-12 15:19:20 -0500

    WIP creating reducer

--- 14/redux-netflix/src/modules/index.js
+++ 14/redux-netflix/src/modules/index.js
@@ -1,6 +1,9 @@
 const {combineReducers} = require('redux');
-const {reducer: movies} = require('./movies');
+const {
+  reducer: movies
+} = require('./movies');
 
 module.exports = combineReducers({
   movies
+  // More reducers go here...
 });
--- 14/redux-netflix/src/modules/movies.js
+++ 14/redux-netflix/src/modules/movies.js
@@ -1,6 +1,28 @@
-const {handelActions} = require('redux-actions');
+const {handleActions} = require('redux-actions');
 
 const FETCH_MOVIES = 'movies/FETCH_MOVIES';
 const FETCH_MOVIE = 'movies/FETCH_MOVIE';
 
-module.exports = {};
+module.exports = {
+  fetchMoviesActionCreator: (movies) => ({
+    type: FETCH_MOVIES,
+    movies
+  }),
+  fetchMovieActionCreator: (index) => ({
+    type: FETCH_MOVIE,
+    index
+  }),
+  reducer: handleActions({
+    [FETCH_MOVIES]: (state, action) => ({
+      ...state,
+      all: action.movies
+    }),
+    [FETCH_MOVIE]: (state, action) => ({
+      ...state,
+      current: state.all[action.index - 1]
+    })
+  }, {
+    movies: [],
+    movie: {}
+  })
+};
