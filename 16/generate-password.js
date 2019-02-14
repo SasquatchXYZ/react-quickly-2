0be5c02489f8fa73197fad0fc47de4261001b968
SasquatchXYZ <dalton.ricker.2011@graduate.usmma.edu>
2019-02-14 13:23:21 -0500

    Jest Testing

---
+++ 16/generate-password.js
@@ -0,0 +1,20 @@
+module.exports = () => (
+  Math.random().toString(36).slice(-8)
+);
+
+/*let random = Math.random();
+console.log(random);
+console.log(random.toString(36).slice(-8));*/
+
+/*
+let randomBinary = Math.random();
+console.log(randomBinary);
+console.log(randomBinary.toString(2));
+
+let randomOctal = Math.random();
+console.log(randomOctal);
+console.log(randomOctal.toString(8));
+
+let randomHex = Math.random();
+console.log(randomHex);
+console.log(randomHex.toString(16));*/
