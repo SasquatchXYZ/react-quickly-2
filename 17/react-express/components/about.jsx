8d33c9cfc578973385a943e4390a1fa09449b2af
SasquatchXYZ <dalton.ricker.2011@graduate.usmma.edu>
2019-02-16 17:44:24 -0500

    this keyboard is tiny..

---
+++ 17/react-express/components/about.jsx
@@ -0,0 +1,9 @@
+const React = require('react');
+
+module.exports = About = () => (
+  <div>
+    <a href="http://Node.University" target="_blank">Node.University</a> is home to top-notch Node education which
+    brings joy to JavaScript engineers.
+  </div>
+)
+
